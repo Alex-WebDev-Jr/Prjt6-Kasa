d45f5c9435fb08528313af7d4172e05a8f8de92d
OpenCeaftravail <eaftravail@hotmail.com>
2023-07-07 04:51:48 +0200

    sass About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import Header from './Header';
 import Footer from './Footer';
 import aboutImage from '../assets/about.png';
+import '../style/About.scss';
 
 function About() {
+  const [collapses, setCollapses] = useState([
+    { id: 1, title: 'Fiabilité', content: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.', isOpen: false },
+    { id: 2, title: 'Respect', content: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.', isOpen: false },
+    { id: 3, title: 'Service', content: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.', isOpen: false },
+    { id: 4, title: 'Sécurité', content: 'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.', isOpen: false },
+  ]);
+
+  const handleCollapseToggle = (id) => {
+    setCollapses((prevCollapses) =>
+      prevCollapses.map((collapse) =>
+        collapse.id === id ? { ...collapse, isOpen: !collapse.isOpen } : collapse
+      )
+    );
+  };
+
   return (
     <div>
       <Header />
       <main>
-      <img src={aboutImage} alt="À Propos" />
+        <img src={aboutImage} alt="À Propos" />
+
+        {collapses.map((collapse) => (
+          <div className={`collapse ${collapse.isOpen ? 'open' : 'closed'}`} key={collapse.id}>
+            <div className="collapse-header" onClick={() => handleCollapseToggle(collapse.id)}>
+              <h3>{collapse.title}</h3>
+              <div className="collapse-arrow">
+                <FontAwesomeIcon icon={faChevronUp} />
+              </div>
+            </div>
+            {collapse.isOpen && (
+              <div className="collapse-content">
+                <p>{collapse.content}</p>
+              </div>
+            )}
+          </div>
+        ))}
       </main>
       <Footer />
     </div>
